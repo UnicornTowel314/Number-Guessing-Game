c1a05f045707b3491c0d8f42a1bdfc6dd08d4d2e
Sarah Mckee <sarahmckee714@gmail.com>
2024-12-14 14:07:20 -0600

    Added greeting output for new users

--- number_guess.sh
+++ number_guess.sh
@@ -6,3 +6,16 @@ RAND_NUM=$(( $RANDOM % 1000 + 1 ))
 
 echo "Enter your username:"
 read USERNAME
+
+PLAYER_ID=$($PSQL "SELECT player_id FROM player_stats WHERE username = '$USERNAME")
+
+if [[ -z $PLAYER_ID ]]
+then
+  # insert player into table
+  INSERT_PLAYER_RESULT=$($PSQL "INSERT INTO player_stats(username) VALUES('$USERNAME')")
+  # welcome player
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  # query table
+  # welcome player
+fi
